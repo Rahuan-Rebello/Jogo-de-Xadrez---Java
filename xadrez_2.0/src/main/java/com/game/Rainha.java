ea9e7e75792d330a00ae3a30bbf74c378be8c889
azedoo <evolker.la.la@gmail.com>
2026-06-10 17:16:14 -0300

    Update Rainha.java

--- xadrez_2.0/src/main/java/com/game/Rainha.java
+++ xadrez_2.0/src/main/java/com/game/Rainha.java
@@ -2,7 +2,7 @@ package com.game;
 
 public class Rainha extends Peca {
 
-    // Construtor da rainha
+
     public Rainha(int linha, int coluna, boolean branca) {
         super(linha, coluna, branca);
     }
@@ -12,7 +12,7 @@ public class Rainha extends Peca {
         int dLinha = Math.abs(novaLinha - linha);
         int dColuna = Math.abs(novaColuna - coluna);
 
-        // Geometria: linha reta OU diagonal
+   
         boolean ehLinhaReta = (novaLinha == linha || novaColuna == coluna);
         boolean ehDiagonal = (dLinha == dColuna);
 
@@ -20,7 +20,6 @@ public class Rainha extends Peca {
 
         if (destinoEhAliado(novaLinha, novaColuna, tabuleiro)) return false;
 
-        // Colisão: o mesmo loop de caminho livre usado na Torre e Bispo
         int stepLinha = Integer.signum(novaLinha - linha);
         int stepColuna = Integer.signum(novaColuna - coluna);
         int l = linha + stepLinha;
