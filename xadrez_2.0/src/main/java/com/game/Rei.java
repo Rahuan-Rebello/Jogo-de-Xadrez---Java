5bce4d991d8cab0eab084ec44cf4eea0ec746b1c
azedoo <evolker.la.la@gmail.com>
2026-06-10 17:16:26 -0300

    Update Rei.java

--- xadrez_2.0/src/main/java/com/game/Rei.java
+++ xadrez_2.0/src/main/java/com/game/Rei.java
@@ -2,7 +2,7 @@ package com.game;
 
 public class Rei extends Peca {
 
-    // Construtor do rei
+
     public Rei(int linha, int coluna, boolean branca) {
         super(linha, coluna, branca);
     }
@@ -12,7 +12,7 @@ public class Rei extends Peca {
         int dLinha = Math.abs(novaLinha - linha);
         int dColuna = Math.abs(novaColuna - coluna);
 
-        // Geometria: no máximo 1 casa em qualquer direção
+        
         if (dLinha > 1 || dColuna > 1) return false;
 
         return !destinoEhAliado(novaLinha, novaColuna, tabuleiro);
