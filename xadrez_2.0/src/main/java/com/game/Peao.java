f8c9322de461c44566ababbe4d87c418286400cd
azedoo <evolker.la.la@gmail.com>
2026-06-10 17:15:46 -0300

    Update Peao.java

--- xadrez_2.0/src/main/java/com/game/Peao.java
+++ xadrez_2.0/src/main/java/com/game/Peao.java
@@ -11,12 +11,11 @@ public class Peao extends Peca {
 public boolean movimentoValido(int novaLinha, int novaColuna, Tabuleiro tabuleiro) {
     int direcao = branca ? -1 : 1; 
 
-    // 1. Andar para frente
     if (novaColuna == coluna && novaLinha == linha + direcao) {
         return !tabuleiro.temPeca(novaLinha, novaColuna);
     }
 
-    // 2. Capturar na diagonal
+
     if (Math.abs(novaColuna - coluna) == 1 && novaLinha == linha + direcao) {
         Peca alvo = tabuleiro.getPeca(novaLinha, novaColuna);
         return alvo != null && alvo.isBranca() != this.isBranca();
