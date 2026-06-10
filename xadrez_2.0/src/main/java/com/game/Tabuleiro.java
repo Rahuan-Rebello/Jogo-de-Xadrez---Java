92c26400a42f919b12d48b45c3cba90f93b96599
azedoo <evolker.la.la@gmail.com>
2026-06-10 17:16:44 -0300

    Update Tabuleiro.java

--- xadrez_2.0/src/main/java/com/game/Tabuleiro.java
+++ xadrez_2.0/src/main/java/com/game/Tabuleiro.java
@@ -4,19 +4,19 @@ public class Tabuleiro {
     private Peca[][] casas = new Peca[8][8];
 
     public void inicializarPecas() {
-        // Coloca Peões
+    
         for (int i = 0; i < 8; i++) {
             casas[6][i] = new Peao(6, i, true);  // Peões brancos
             casas[1][i] = new Peao(1, i, false); // Peões pretos
         }
 
-        // Coloca peças da linha principal (Brancas na linha 7, Pretas na linha 0)
+   
         colocarLinhaPrincipal(7, true);
         colocarLinhaPrincipal(0, false);
     }
 
     private void colocarLinhaPrincipal(int linha, boolean branca) {
-        // Definindo as colunas explicitamente
+       
         casas[linha][0] = new Torre(linha, 0, branca);
         casas[linha][1] = new Cavalo(linha, 1, branca);
         casas[linha][2] = new Bispo(linha, 2, branca);
@@ -37,11 +37,11 @@ public class Tabuleiro {
         }
 
         public boolean temPeca(int linha, int coluna) {
-        // Verifica se a posição está dentro do tabuleiro e se a casa não é nula
+     
         return linha >= 0 && linha < 8 && coluna >= 0 && coluna < 8 && casas[linha][coluna] != null;
     }
 
-    // Adicione isto na sua classe Tabuleiro
+
     public boolean contemRei(boolean corBranca) {
     for (int l = 0; l < 8; l++) {
         for (int c = 0; c < 8; c++) {
