71062901e89b9a9a5d9b5fea22675c9f0ce460ae
azedoo <evolker.la.la@gmail.com>
2026-06-10 17:14:06 -0300

    Update Bispo.java

--- xadrez_2.0/src/main/java/com/game/Bispo.java
+++ xadrez_2.0/src/main/java/com/game/Bispo.java
@@ -2,23 +2,23 @@ package com.game;
 
 public class Bispo extends Peca {
 
-    // Construtor do bispo
+    
     public Bispo(int linha, int coluna, boolean branca) {
         super(linha, coluna, branca);
     }
 
    @Override
     public boolean movimentoValido(int novaLinha, int novaColuna, Tabuleiro tabuleiro) {
-        // Geometria: Diferença de linha deve ser igual à diferença de coluna
+
         int dLinha = Math.abs(novaLinha - linha);
         int dColuna = Math.abs(novaColuna - coluna);
         if (dLinha != dColuna) return false;
 
-            // Chamada correta:
+
         if (destinoEhAliado(novaLinha, novaColuna, tabuleiro)) {
             return false;
         }
-        // Colisão (caminho livre na diagonal)
+     
         int stepLinha = Integer.signum(novaLinha - linha);
         int stepColuna = Integer.signum(novaColuna - coluna);
         int l = linha + stepLinha;
