6540c4265047663deb8d9d00e04b2448bf4bd998
azedoo <evolker.la.la@gmail.com>
2026-06-10 17:15:58 -0300

    Update Peca.java

--- xadrez_2.0/src/main/java/com/game/Peca.java
+++ xadrez_2.0/src/main/java/com/game/Peca.java
@@ -6,7 +6,7 @@ public abstract class Peca {
 
     public Peca(int l, int c, boolean b) { this.linha = l; this.coluna = c; this.branca = b; }
 
-    // Método abstrato único (definido apenas uma vez)
+   
     public abstract boolean movimentoValido(int novaLinha, int novaColuna, Tabuleiro tabuleiro);
 
     public void mover(int nl, int nc) { this.linha = nl; this.coluna = nc; }
